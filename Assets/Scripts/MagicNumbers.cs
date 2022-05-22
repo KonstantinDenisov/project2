f9abb8d1695b5d5e30a473959faa6d2386b3e934
KonstantinDenisov <omacs@mail.ru>
2022-05-22 03:12:06 +0300

    MagicNumbers

--- Assets/Scripts/MagicNumbers.cs
+++ Assets/Scripts/MagicNumbers.cs
@@ -1,17 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MagicNumbers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int _min;
+    [SerializeField] private int _max;
+
+    [SerializeField] private TextMeshProUGUI InfoLabel;
+    [SerializeField] private TextMeshProUGUI GuessLable;
+    [SerializeField] private Button MoreButton;
+    [SerializeField] private Button SmallerButton;
+    [SerializeField] private Button RightButton;
+
+    private int _guess;
+
+    private void Start()
     {
-        Debug.Log("start");
+        SetInfoText($"загадай число от {_min} до {_max}");
+        CalculateGuess();
+        MoreButton.onClick.AddListener(MoreBottonClicked);
+        SmallerButton.onClick.AddListener(SmallerBottonClicked);
+        RightButton.onClick.AddListener(RightBottonClicked);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
     }
+
+    private void CalculateGuess()
+    {
+        _guess = (_min + _max) / 2;
+        SetInfoGuessText($"твоё число: {_guess}?");
+    }
+
+    private void SetInfoText(string text)
+    {
+        Debug.Log(text);
+        InfoLabel.text = text;
+    }
+    
+    private void SetInfoGuessText(string text)
+    {
+        Debug.Log(text);
+        GuessLable.text = text;
+    }
+
+    private void MoreBottonClicked ()
+    {
+        SetInfoText("число больше");
+        _min = _guess;
+        CalculateGuess();
+    }
+    private void RightBottonClicked ()
+    {
+        SetInfoText($"ПОБЕДА !!!! твоё число {_guess}"); 
+    }
+    private void SmallerBottonClicked ()
+    {
+        SetInfoText("число меньше");
+        _max = _guess;
+        CalculateGuess();
+    }
+    
 }
